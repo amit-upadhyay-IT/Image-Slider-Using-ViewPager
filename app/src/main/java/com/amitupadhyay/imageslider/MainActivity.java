e6517e7dec52a3f0e6ae9ad99ccf84be1b8fbaf4
amit-upadhyay-IT <youarefreak1@gmail.com>
2016-08-13 00:10:07 +0530

    initial commit

---
+++ app/src/main/java/com/amitupadhyay/imageslider/MainActivity.java
@@ -0,0 +1,22 @@
+package com.amitupadhyay.imageslider;
+
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    CustomAdapter adapter;
+    ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        adapter = new CustomAdapter(this);
+        viewPager.setAdapter(adapter);
+
+    }
+}
